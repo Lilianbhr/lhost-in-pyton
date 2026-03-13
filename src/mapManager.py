280f72935e9b516da3554afa8463fcd400dc28bb
Lilian <lil.bouhier@gmail.com>
2026-03-13 13:23:13 +0100

    Dev-v0.2.2
    * v0.2.2-Documentation

--- src/mapManager.py
+++ src/mapManager.py
@@ -9,9 +9,9 @@ class MapManager:
         self.screen_size = screen_size
         self.tmx_data = None
         self.group = self.set_map("map_1")
-        self.focus((320, 320))
 
     def set_map(self, map_name: str) -> pyscroll.PyscrollGroup:
+        """ Permet de charger une map """
         self.tmx_data = load_pygame(self.map_dir + map_name + ".tmx")
         map_layer = pyscroll.BufferedRenderer(
             data=pyscroll.TiledMapData(self.tmx_data),
@@ -23,7 +23,7 @@ class MapManager:
     def get_spawn_point(self):
         for obj in self.tmx_data.objects:
             if obj.name == "spawn_point":
-                return obj.x, obj.y
+                return [obj.x, obj.y]
         return 0, 0
 
     def get_map_size(self):
--- src/sprite.py
+++ src/sprite.py
@@ -2,13 +2,16 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
+    """ Première version d'un personnage ocntrollable par le joueur """
     def __init__(self):
         super().__init__()
+
+        # Visuel + collisions
         self.image = pygame.Surface((32, 64))
         self.image.fill((0, 0, 255))
-
         self.rect = self.image.get_rect()
 
+        # Position + Déplacements
         self.directions = {
             "z": pygame.math.Vector2(0, -1),
             "s": pygame.math.Vector2(0, 1),
@@ -22,30 +25,44 @@ class Player(pygame.sprite.Sprite):
         screen.blit(self.image, self.rect)
 
     def update(self, active_inputs: set, screen_size: tuple):
+        """
+        le mouvement est basé sur une somme de vecteurs,
+        cela permet une gestion efficace de déplacements multidirectionnels
+        notamment sur les diagonales
+        """
         final_direction = pygame.math.Vector2(0, 0)
 
+        # Récupération des forces actives
         for key in self.directions.keys():
             if key in active_inputs:
                 final_direction.x += self.directions[key].x
                 final_direction.y += self.directions[key].y
 
+        # Normalisation
         if final_direction.xy != (0, 0):
             final_direction = final_direction.normalize()
 
+        # déplacement précis
         self.precise_pos[0] += final_direction.x * self.speed
         self.precise_pos[1] += final_direction.y * self.speed
 
+        # empêcher le joueur de sortir de la carte
+        # gauche
         if self.precise_pos[0] < 0:
             self.precise_pos[0] = 0
 
+        # droite
         elif self.precise_pos[0] + self.rect.width > screen_size[0]:
             self.precise_pos[0] = screen_size[0] - self.rect.width
 
+        # haut
         if self.precise_pos[1] < 0:
             self.precise_pos[1] = 0
 
+        # bas
         elif self.precise_pos[1] + self.rect.height > screen_size[1]:
             self.precise_pos[1] = screen_size[1] - self.rect.height
 
+        # convertion en coordonnées entières
         self.rect.topleft = (round(self.precise_pos[0]),
                              round(self.precise_pos[1]))
