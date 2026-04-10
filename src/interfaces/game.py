2da26873b452ef5f055439a8d833cfff3d5841b4
Lilian <lil.bouhier@gmail.com>
2026-04-10 13:25:25 +0200

    ADD: spawn_point dynamiques

--- src/interfaces/game.py
+++ src/interfaces/game.py
@@ -33,7 +33,10 @@ class Game(Interface):
         for portal in self.map_manager.portals:
             if self.player.rect.colliderect(portal[0]):
                 self.map_manager.change_map(portal[1])
-                self.player.rect.topleft = self.map_manager.get_spawn_point()
+                self.player.rect.topleft = self.map_manager.get_spawn_point(
+                    self.map_manager.map_name
+                )
+                self.map_manager.map_name = portal[1]
                 self.map_manager.group.add(self.player)
 
         self.map_manager.focus(self.player.rect.center)
